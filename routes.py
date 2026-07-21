1d7c6e49953ccda71520b0c44a2d6557d11b00d3
Polyston <143893612+Polyston@users.noreply.github.com>
2026-07-21 13:06:33 +0100

    Add an endpoint to update a single todo by id

--- routes.py
+++ routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status, HTTPException
 from schemas import TodoItem, TodoItemResponse
 from database import TodoList
 from bson import ObjectId
+from pymongo import ReturnDocument
 
 # Stores routes for todo-list operations
 todo_router = APIRouter()
@@ -23,7 +24,7 @@ def create_todo_entry(Todo: TodoItem):
     return {"id": str(result.inserted_id), "title": todo_document["title"], "description": todo_document["description"]}
 
 # Endpoint used to get a single todo entry by id
-@todo_router.get("/todos{id}", response_model=TodoItemResponse, status_code=status.HTTP_200_OK)
+@todo_router.get("/todos/{id}", response_model=TodoItemResponse, status_code=status.HTTP_200_OK)
 def get_todo_entry(id: str):
 
     # Stores the document with the desired todo entry if the passed id matches an existing id
@@ -55,3 +56,30 @@ def get_all_todo_entries():
     
     # The todo entries retrieved from the TodoList collection have their data validated by the response model and are returned
     return retrieved_todo_documents
+
+# Endpoint used to update a single todo by id
+@todo_router.put("/todos/{id}", response_model=TodoItemResponse, status_code=status.HTTP_200_OK)
+def update_todo_entry(id: str, updated_todo: TodoItem):
+
+    # Stores the document with the desired todo entry if the passed id matches an existing id
+    updated_todo_document = TodoList.find_one({"_id": ObjectId(id)})
+
+    # If the id passed is not found, updated_todo_document is empty and an error is raised
+    if not updated_todo_document:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="The entered todo id does not exist")
+    
+    # The document which has an id that matches the passed id is updated
+    updated_todo_document = TodoList.find_one_and_update(
+        {"_id": ObjectId(id)},
+        {"$set": 
+            {
+                "title": updated_todo.title,
+                "description": updated_todo.description 
+            }
+    
+        },
+        return_document=ReturnDocument.AFTER
+    )
+    
+    # The updated todo entry has its data validated by the response model and is then returned
+    return {"id": str(updated_todo_document["_id"]), "title": updated_todo_document["title"], "description": updated_todo_document["description"]}
